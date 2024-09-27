bb0634c6c81b7e94eb4ff3500354ae3b27131b56
Matheus Gois <mattheusgoiis@gmail.com>
2024-09-27 03:40:07 -0300

    add remove button

--- src/scripts/edit_form.js
+++ src/scripts/edit_form.js
@@ -73,11 +73,23 @@ loadForm(id).then(formData => {
                 </svg>
             `;
         
+            let removeButton = document.createElement('div');
+            removeButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+            `;
+            removeButton.setAttribute('class', 'remove-button');
+            removeButton.addEventListener('click', function() {
+                newField.remove();
+            });
+
+            if (currentCount > 0) {
+                newField.appendChild(removeButton);
+            }
             newField.appendChild(inputName);
             newField.appendChild(selectType);
             if (field.type !== 'single_choice' && field.type !== 'multiple_choice') {
                 newField.appendChild(inputResponse);
-            }
+            }            
 
 
             if (field.options) {
--- src/scripts/functions/addNewField.js
+++ src/scripts/functions/addNewField.js
@@ -6,6 +6,15 @@ const addNewField = () => {
     let newField = document.createElement('div');
     newField.classList.add('form-field');
 
+    let removeButton = document.createElement('div');
+    removeButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+    `;
+    removeButton.setAttribute('class', 'remove-button');
+    removeButton.addEventListener('click', function() {
+        newField.remove();
+    });
+
     let inputName = document.createElement('input');
     inputName.type = 'text';
     inputName.placeholder = 'Nome do campo';
@@ -47,6 +56,7 @@ const addNewField = () => {
         </svg>
     `;
 
+    newField.appendChild(removeButton);
     newField.appendChild(inputName);
     newField.appendChild(selectType);
     newField.appendChild(inputResponse);
